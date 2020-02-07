ebf3186114ca90d131be77980f3b7a8bcbe3728b
Thiaguz <zthiagow@gmail.com>
2020-02-07 07:30:04 -0300

    Add vue-meta dependency

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import Meta from 'vue-meta';
+
 import App from './App.vue';
 import router from './router';
 
 import 'normalize.css';
+
+Vue.use(Meta);
 
 Vue.config.productionTip = false;
 
